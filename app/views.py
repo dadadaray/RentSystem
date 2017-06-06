d840d2cfb60de4655e0890a95762581deedf4a15
sunxiaohui1996 <3290465792@qq.com>
2017-06-06 15:27:16 +0800

    完善登录功能，登录验证

--- app/views.py
+++ app/views.py
@@ -3,10 +3,19 @@ from flask import Flask, render_template,url_for,request
 from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
-from flask import redirect
+from flask import redirect, make_response, flash
 from DB.BaseDB import BaseDB
+import datetime
+import sys
+
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
 
 from enity.User import User
+
+DB = BaseDB()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -22,11 +31,62 @@ def hello_world():
 
 @app.route("/index")
 def index():
-    return 'index'
+    return render_template("index.html")
 
-@app.route("/login")
+@app.route("/rent.csv")
+def findCsv():
+    return render_template("rent.csv")
+
+@app.route("/denglu")
+def denglu():
+    return render_template("login.html")
+
+@app.route("/zhuce")
+def zhuce():
+    return render_template("register.html")
+
+@app.route("/login", methods=['POST','GET'])
 def login():
-    return 'index'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        name = request.cookies.get(username)
+        #如果没有登录
+        if name == None:
+            obj1 = DB.search_User(User,username)
+            #用户名不存在
+            if obj1 == None:
+                flash("用户名不存在，请重新登陆！")
+            #用户名存在
+            else:
+                #密码正确
+                if password == obj1.password:
+                    resp = make_response(render_template("index.html"))
+                    outdate = datetime.datetime.today()+datetime.timedelta(days=7)
+                    resp.set_cookie(username,username,expires=outdate)
+                    #获取当前登录用户名
+                    na = request.cookies.get('username')
+                    print("na:",na)
+                    #当前登录用户不为空，登出
+                    if na != None:
+                        resp.delete_cookie(na)
+                    #更新当前登录用户名
+                    resp.set_cookie('username',username)
+                    return resp
+                #密码错误
+                else:
+                    flash("密码错误，请重新登陆！")
+        #已登录
+        else:
+            flash("您已登录，请勿重复登录！")
+
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    resp = make_response("delete cookie")
+    resp.delete_cookie('name')
+    return resp
 
 @app.route("/register", methods=['POST','GET'])
 def register():
@@ -38,9 +98,8 @@ def register():
 
         print("username:%s password:%s repw:%s" % (username,password,repw))
         user = User(userName=username,password=password)
-        DB = BaseDB()
         DB.insert_into_table(user)
-    return render_template('register.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
