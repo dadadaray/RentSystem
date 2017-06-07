65b333d594784f6d8b802235b805b6d9be833a07
fangziyi <805659107@qq.com>
2017-06-07 09:49:14 +0800

    保存工作

--- app/views.py
+++ app/views.py
@@ -8,6 +8,8 @@ from DB.BaseDB import BaseDB
 
 from enity.User import User
 
+DB = BaseDB()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
@@ -15,18 +17,41 @@ class NameForm(Form):
     name = StringField('What is your name',validators=[Required()])
     submit = SubmitField('Submit')
 
-
+@app.route("/history")
+def history():
+    return render_template("history.html")
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
 @app.route("/index")
 def index():
-    return 'index'
+    return render_template("index.html")
 
-@app.route("/login")
+@app.route("/denglu")
+def denglu():
+    return render_template("login.html")
+
+@app.route("/zhuce")
+def zhuce():
+    return render_template("register.html")
+
+@app.route("/login", methods=['POST','GET'])
 def login():
-    return 'index'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        obj1 = DB.search_User(User,username)
+        if password == obj1.password:
+            print(password,obj1.password)
+        else:
+            print("cuole")
+
+        return render_template('index.html')
+@app.route("/rent.csv")
+def findCsv():
+    return render_template("rent.csv")
 
 @app.route("/register", methods=['POST','GET'])
 def register():
@@ -38,9 +63,8 @@ def register():
 
         print("username:%s password:%s repw:%s" % (username,password,repw))
         user = User(userName=username,password=password)
-        DB = BaseDB()
         DB.insert_into_table(user)
-    return render_template('register.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
