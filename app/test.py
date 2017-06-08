82cd43d62e0336eb92095fa2ee48faad292041de
fangziyi <805659107@qq.com>
2017-06-08 16:16:14 +0800

    重建数据库表

--- app/test.py
+++ app/test.py
@@ -1,17 +1,11 @@
 # -*- coding: UTF-8 -*-
 from DB.BaseDB import BaseDB
-from enity.User import User
-from enity.History import History
+
 #创建表
 DB = BaseDB()
 DB.create_table()
 
-obj = DB.search_by_id(User,1)
-obj1 = DB.search_by_param(User,'haha')
-obj2 = DB.search_userid(self,History,1)
-obj3 = DB.search_userid(self,History,2)
-
-print(obj)
-print("one",obj.userName,obj.password)
-print("two",obj1.userName,obj1.password)
-print("1",obj2.way,obj2.userId,obj2.locationCity,obj2.goalCity)
+# obj2 = DB.search_userid(History,1)
+# obj3 = DB.search_userid(History,2)
+#
+# print("1",obj2.way,obj2.userId,obj2.locationCity,obj2.goalCity)
