a5c3e5f77cb599a2fd44fe10909f0c8db40ef838
tonghaiping <2362264483@qq.com>
2017-06-08 17:13:14 +0800

    无操作

--- app/test.py
+++ app/test.py
@@ -1,14 +1,12 @@
 # -*- coding: UTF-8 -*-
-from app.DB.BaseDB import BaseDB
+from DB.BaseDB import BaseDB
+from enity.History import History
+from enity.Record import Record
 from enity.User import User
 
 #创建表
 DB = BaseDB()
 DB.create_table()
 
-obj = DB.search_by_id(User,1)
-obj1 = DB.search_by_param(User,'haha')
 
-print(obj)
-print("one",obj.userName,obj.password)
-print("two",obj1.userName,obj1.password)
+
